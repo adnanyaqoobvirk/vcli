f3640cff4b3a2f582a86d365f3ce6c9ac1c53e90
Adnan Yaqoob Virk <adnan@yaqoob.org>
2016-05-03 16:32:00 +0500

    again chagned the requirements for travis testing

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         'prompt_toolkit==0.57',
         'Pygments >= 2.0',  # Pygments has to be Capitalcased. WTF?
         'sqlparse == 0.1.16',
-        'vertica-python==0.6.1',
+        'vertica-python >= 0.6.1',
         'setproctitle >= 1.1.9'
     ],
     entry_points='''
@@ -41,10 +41,7 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: Unix',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: SQL',
